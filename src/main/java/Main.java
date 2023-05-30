6e05c7e52eb70b20fba7371243f76d40b20468e4
Scurpa <133747714+Scurpa@users.noreply.github.com>
2023-05-30 22:52:23 +0200

    Update Main.java
    car3 is white now

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
-        Car car3 = new Car("blue", "VW", 3);
+        Car car3 = new Car("white", "VW", 3);
 	
 
         factory.buildCar(car1);
