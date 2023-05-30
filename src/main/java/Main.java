92ea49a50a610463972e82e688391088d1494ab4
AnnBori <s4anbori@uni-trier.de>
2023-05-30 21:03:34 +0200

    added car4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,11 @@ public class Main {
         Car car1 = new Car("red", "BMW", 5);
         Car car2 = new Car("blue", "Audi", 3);
         Car car3 = new Car("blue", "VW", 3);
+		Car car4 = new Car("red", "VW", 3);
 
         factory.buildCar(car1);
         factory.buildCar(car2);
-	  factory.buildCar(car2);	
+	  factory.buildCar(car3);
+		factory.buildCar(car4);
     }
 }
